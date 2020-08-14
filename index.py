03910826d5d15a47192158f26405e3a85ef64a8a
zackprogrammer <63200308+zackprogrammer@users.noreply.github.com>
2020-08-14 12:15:49 -0400

    'LOL'

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
 # Will import the socket stuff later
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hi"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
